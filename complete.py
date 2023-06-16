bd32914888ce8f865d0a6443f339fcd16c61e9a7
David Völgyes <volgyes@stcorp.no>
2023-06-16 10:00:11 +0200

    addd

--- complete.py
+++ complete.py
@@ -2,22 +2,29 @@
 import os
 import openai
 import click
+from pathlib import Path
 
 @click.command()
 @click.option('-m', '--model', default='text-davinci-003', help='The model to use for generating the response.')
 @click.argument('prompt')
-def main(prompt, model):
+@click.option('-t','--text', type=str, default=None)
+def main(prompt, model,text):
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
+    if text:
+        text = Path(text).read_text()
+
+        prompt = prompt + text
+
     response = openai.Completion.create(
-        model=model,
-        prompt=prompt,
-        temperature=0,
-        max_tokens=1024,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+            model=model,
+            prompt=text,
+            temperature=2.0,
+            max_tokens=1024,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
 
     print(response.choices[0].text.strip())
 
